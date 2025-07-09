56d901b936071d7cd7c1748108b27e544a52c1b4
YayanLI <aynlai99@gmail.com>
2025-07-09 11:23:58 +0200

    gazebo

--- crab_model/launch/rviz.launch.py
+++ crab_model/launch/rviz.launch.py
@@ -3,7 +3,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, Command
+from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
@@ -14,7 +14,6 @@ def generate_launch_description():
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
-    use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('crab_model'))
@@ -26,7 +25,7 @@ def generate_launch_description():
 
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-    node_robot_state_publisher = Node(
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
@@ -50,6 +49,7 @@ def generate_launch_description():
     )
 
 
+
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -62,7 +62,7 @@ def generate_launch_description():
             description = 'Use ros2_control if true'
         ),
 
-        node_robot_state_publisher,
+        robot_state_publisher,
         joint_state_publisher_gui,
         rviz_node
     ])
